50a41bb99dce43bcf5e819db0b61aae841bb00e2
mosolini <mosolini@gmail.com>
2017-08-22 10:53:57 -0700

    Added DB deployment
    - Maven config for DB deployment
    - Added the sql files to create the DB schema

---
+++ posManagementBoot/src/DB/data.sql
@@ -0,0 +1,30 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
+
+/********************* settings *********************/
+INSERT INTO public.setting(
+	setting_name, setting_value)
+	VALUES ('customerName', 'NSGB');
+	
+INSERT INTO public.setting(
+	setting_name, setting_value)
+	VALUES ('vendorName', 'EBE');
+	
+INSERT INTO public.setting(
+	setting_name, setting_value)
+	VALUES ('customerClass', 'A');
+
+/******************** Security ********************/
+INSERT INTO public.role(
+	role_id, role_name)
+	VALUES (1, 'ADMIN');
+
+INSERT INTO public.role(
+	role_id, role_name)
+	VALUES (2, 'SUPERUSER');
+
+INSERT INTO public.role(
+	role_id, role_name)
+	VALUES (3, 'USER');	
+	
