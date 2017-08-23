858c5826dbd2f460bad9566d089dd58920898da9
mosolini <mosolini@gmail.com>
2017-08-23 11:56:54 -0700

    Updated DB deployment plugin

--- posManagementBoot/src/DB/data.sql
+++ posManagementBoot/src/DB/data.sql
@@ -15,6 +15,14 @@ INSERT INTO public.setting(
 	setting_name, setting_value)
 	VALUES ('customerClass', 'A');
 
+/********************* Pos Condition *********************/
+INSERT INTO public.pos_condition(
+	pos_condition_id, pos_condition_name)
+	VALUES (1, 'New');
+INSERT INTO public.pos_condition(
+	pos_condition_id, pos_condition_name)
+	VALUES (2, 'Used');
+
 /******************** Security ********************/
 INSERT INTO public.role(
 	role_id, role_name)
